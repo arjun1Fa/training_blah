c50612fb23bdc4376365b4b420d32cdeaf60a9e0
arjun1Fa <culerarjun@gmail.com>
2026-07-15 14:27:06 +0530

    Add multiplication and even number printing functionality

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number: "))
+i=1
+while i<11:
+    p = a*i
+    print(p)
+    i = i+1
---
+++ onetofive.py
@@ -0,0 +1,9 @@
+i=1
+j = 0
+while True:
+    if i >10:
+        break
+    if i%2==0:
+        j = i
+        print(j)
+    i = i+1
